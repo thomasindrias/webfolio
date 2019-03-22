b781263ac5f157abbdf92b0b45540b00b500a894
thomasindrias <thoin216@student.liu.se>
2019-03-22 22:55:01 +0100

    seeecret

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section class="hero is-primary has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            <nuxt-link to="/">
+              Thomas Indrias
+            </nuxt-link>
+          </h1>
+          <h2 class="subtitle">
+            Welcome to my portfolio!
+          </h2>
+        </div>
+      </div>
+    </section>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,103 @@
+require('dotenv').config()
+// eslint-disable-next-line nuxt/no-cjs-in-config
+const pkg = require('./package')
+// eslint-disable-next-line nuxt/no-cjs-in-config
+const client = require('./plugins/contentful')
+
+// eslint-disable-next-line nuxt/no-cjs-in-config
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: ['~/plugins/contentful'],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
+    '@nuxtjs/bulma',
+    '@nuxtjs/pwa',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/markdownit'
+  ],
+
+  /*
+  ** Markdownit configuration
+  */
+  markdownit: {
+    injected: true
+  },
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  generate: {
+    routes() {
+      return client.getEntries({ content_type: 'post' }).then(entries => {
+        return entries.items.map(entry => {
+          return {
+            route: entry.fields.slug,
+            payload: entry
+          }
+        })
+      })
+    }
+  }
+}
